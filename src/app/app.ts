b28939f43da3c00f4ead3e6301b71ee302a89431
MdTowhidurRahmanSamin <samin@shantaholdings.com>
2026-05-05 15:59:15 +0600

    commit

---
+++ src/app/app.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.html'
+})
+export class App implements OnInit {
+  readonly API_URL = 'https://localhost:7242/WeatherForecast';
+  public data: any[] = [];
+  
+  newForecast = {
+    date: new Date().toISOString().split('T')[0],
+    temperatureC: 0,
+    summary: '' 
+  };
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.refresh();
+  }
+
+  refresh() {
+    this.http.get<any[]>(this.API_URL).subscribe((res: any)=>{
+      this.data = res
+      this.cdr.detectChanges();
+    })
+  }
+
+  create() {
+    this.http.post(this.API_URL, this.newForecast).subscribe(() => {
+      this.refresh(); 
+      this.newForecast.summary = ''; 
+    });
+  }
+}
